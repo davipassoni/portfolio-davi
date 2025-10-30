46f9d15500fd4214734eaf5e3d263ebf15518cf3
davi_passoni <passonidavi9@gmail.com>
2025-10-30 11:32:49 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,30 @@ function App() {
 
   return (
     <>
-      <h1 classname={styles.titulo}>bob do creme  </h1>
+    <nav> classname={ styles.menu    }
+      
+    <a href="#s1">home</a>
+    <a href="#s2">cards</a>
+    <a href="#s3">video</a>
+    <a href="#s4">contato</a>
+    </nav>
+    <main>
+    <section>
+      <h1>sessao 1</h1>
+    </section>
+    <section>
+      <h2>sessao 2</h2>
+    </section>
+    <section>
+      <h3>sessao 3</h3>
+    </section>
+    <section>
+      <h4>sessao 4</h4>
+    </section>
+
+
+
+    </main>
     </>
   )
 }
